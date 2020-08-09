5d3b2d1f0e3918b3aa28de74c7271cf4e9013c18
Mohammad Suhaib Ahmed <suhaib.md06@gmail.com>
2020-08-09 11:05:09 +0530

    Start of Lesson 9 API Calls

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,7 +6,9 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users" :width="20" :height="20"
+        ><span>{{ event.attendees.length }} attending</span></BaseIcon
+      >
     </div>
   </router-link>
 </template>
