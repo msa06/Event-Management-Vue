7bbce21f573ff262cdd1bfc29ffe4ba713b21b92
Mohammad Suhaib Ahmed <suhaib.md06@gmail.com>
2020-08-12 14:40:15 +0530

    Added More Global Component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select type="text" @input="updateValue" :value="value" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
